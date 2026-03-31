b708de9c99242d27a4118214f71620571898abaa
40in <anton.sorokin030529@gmail.com>
2026-04-01 02:18:18 +0300

    Architecture init

---
+++ build/x64-Debug/CMakeFiles/fc-tmp/imgui-sfml/download.cmake
@@ -0,0 +1,12 @@
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+message(VERBOSE "Executing download step for imgui-sfml")
+
+block(SCOPE_FOR VARIABLES)
+
+include("D:/dev/game_engine_study/build/x64-Debug/CMakeFiles/fc-stamp/imgui-sfml/download-imgui-sfml.cmake")
+include("D:/dev/game_engine_study/build/x64-Debug/CMakeFiles/fc-stamp/imgui-sfml/verify-imgui-sfml.cmake")
+include("D:/dev/game_engine_study/build/x64-Debug/CMakeFiles/fc-stamp/imgui-sfml/extract-imgui-sfml.cmake")
+
+
+endblock()
---
+++ build/x64-Debug/CMakeFiles/fc-tmp/imgui/download.cmake
@@ -0,0 +1,12 @@
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+message(VERBOSE "Executing download step for imgui")
+
+block(SCOPE_FOR VARIABLES)
+
+include("D:/dev/game_engine_study/build/x64-Debug/CMakeFiles/fc-stamp/imgui/download-imgui.cmake")
+include("D:/dev/game_engine_study/build/x64-Debug/CMakeFiles/fc-stamp/imgui/verify-imgui.cmake")
+include("D:/dev/game_engine_study/build/x64-Debug/CMakeFiles/fc-stamp/imgui/extract-imgui.cmake")
+
+
+endblock()
